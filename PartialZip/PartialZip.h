3a52a5858e24e31f527989e30f76840400c99baa
Allan Kerr <allankerr@Allans-MacBook-Pro.local>
2015-06-29 23:03:33 -0600

    Initial Commit

---
+++ PartialZip/PartialZip.h
@@ -0,0 +1,19 @@
+//
+//  PartialZip.h
+//  PartialZip
+//
+//  Created by Allan Kerr on 2015-06-29.
+//  Copyright (c) 2015 Allan Kerr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PartialZip.
+FOUNDATION_EXPORT double PartialZipVersionNumber;
+
+//! Project version string for PartialZip.
+FOUNDATION_EXPORT const unsigned char PartialZipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PartialZip/PublicHeader.h>
+
+
